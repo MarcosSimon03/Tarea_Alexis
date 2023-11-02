afe1a5f1005c97da3dea57658af1201fb6d10906
MarcosSimon03 <marsimcab3@alu.edu.gva.es>
2023-11-02 12:36:21 +0100

    Ejercicio1 bueno

---
+++ Ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+$variable1 = 987;
+$variable2 = 654;
+$variable3 = 4321;
+echo "";
+echo "";
+echo "El valor de la Variable1 es: $variable1 , El valor de la Variable2 es:  $variable1 , y El valor de la Variable3 es:  $variable3";
+echo "";
+echo "";
+?>
